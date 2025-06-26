6e4ba81f17bace6f845d8782de6089912c754cca
sabitertan <sabitertan@gmail.com>
2025-06-26 19:22:01 -0400

    Latest fixes for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/gito-pong/', // Set to your repo name for GitHub Pages
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        pong: resolve(__dirname, 'src/games/pong/pong.html'),
+        supermario: resolve(__dirname, 'src/games/supermario/supermario.html'),
+        tictactoe: resolve(__dirname, 'src/games/tictactoe/tictactoe.html'),
+      }
+    }
+  }
 });
